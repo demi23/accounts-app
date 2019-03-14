d0f8c148e4e43307bc2b971500bec5e35f194e17
demi23 <48230158+demi23@users.noreply.github.com>
2019-03-14 14:43:28 +0800

    Add files via upload

---
+++ precache-manifest.35ea80211ea1b692febd24c87b1bb766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f42df83476016ccdf4",
+    "url": "/accounts-app/static/css/main.105a068e.chunk.css"
+  },
+  {
+    "revision": "09f42df83476016ccdf4",
+    "url": "/accounts-app/static/js/main.09f42df8.chunk.js"
+  },
+  {
+    "revision": "041cb148b6f15cff9ce5",
+    "url": "/accounts-app/static/js/1.041cb148.chunk.js"
+  },
+  {
+    "revision": "d8dcfe257e964d5b25c9",
+    "url": "/accounts-app/static/js/runtime~main.d8dcfe25.js"
+  },
+  {
+    "revision": "2c360a6f8d522500a03c7e3b88f3b2c8",
+    "url": "/accounts-app/index.html"
+  }
+];
